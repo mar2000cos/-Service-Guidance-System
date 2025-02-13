67658b1c11f26bcc121010e766606788271f5e6e
Marcos Paulo Xavier <75039118+mar2000cos@users.noreply.github.com>
2025-02-13 13:22:17 -0300

    commit

--- category.py
+++ category.py
@@ -2,7 +2,7 @@
 troca_de_teclado = ['teclado disparando tecla', 'teclado com teclas que não funcionam', 'teclado não funciona', 'teclado não funciona', 'teclado com teclas falhando', 'teclado com teclas faltando']
 troca_de_tela = ['substituição de tela', 'troca de tela', 'tela trincada', 'tela rachada', 'tela com manchas', 'tela com linhas', 'tela desfocada']
 formatação = ['sistema não carrega', 'sistema não abre', 'sistema nao entra', 'não entra no sistema', 'restaurar sistema', 'reset do sistema']
-problemas_de_placa = ['equipamento fica com led acesso', 'equipamento não liga', 'equipamento não dá sinal', '']
+problemas_de_placa = ['equipamento fica com led da bateria aceso mas não liga', 'equipamento não liga', 'equipamento não dá sinal', '']
 
 # service mapping dictionary
 category = {
